a99de5356dfac100db87678bf83c1232c4a3efff
bear2u@naver.com <kangse1004>
2019-02-02 02:47:26 +0900

    change kidsnot

--- crawling/kidsnote.py
+++ crawling/kidsnote.py
@@ -2,8 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as E
+from selenium.common.exceptions import NoSuchElementException 
 from dotenv import load_dotenv
 import os
+
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
 
 def every_downloads_chrome(driver):
     if not driver.current_url.startswith("chrome://downloads"):
@@ -30,7 +38,7 @@ options.add_argument('window-size=1920x1080')
 options.add_argument('--disable-gpu')
 
 options.add_experimental_option("prefs", {
-  "download.default_directory": r"D:\workspace_python_study\automation\downloads",
+  "download.default_directory": r"d:\kidsnot\downloads",
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
@@ -68,30 +76,34 @@ albumClick.click()
 print('3. 앨범을 클릭');
 driver.save_screenshot("kidsnote_imgs/3_kids_note_click_album.png")
 
+## 배열이 되어야 한다.
+
 print('4. 리스트 중 처음 껄로 선택')
 contents = driver.find_element_by_class_name("album-list-wrapper")
 # firstListItem = driver.find_element_by_xpath("//a[contains(@href,'req=/albums/')]")
 
+## 배열이 되어야 한다.
 items = contents.find_elements_by_tag_name("a")
 # items[0].click()
+album_idx = 1
 for item in items:
     # print(idx + ' 번째 실행')
-    driver.execute_script("arguments[0].click();", item)
+    alubm_item = driver.find_element_by_xpath("//div[@class='album-list-wrapper']//a["+str(album_idx)+"]")
+    driver.execute_script("arguments[0].click();", alubm_item)
     driver.save_screenshot("kidsnote_imgs/4_kids_note_click_item.png")
 
     driver.implicitly_wait(3)
+        
+    if check_exists_by_xpath("//div[@class='role-name-select-wrapper']"):
+        print('5. 호칭 선택')        
+        nicknames_form = driver.find_element_by_xpath('//div[@class="role-name-select-wrapper"]//div//div[1]/form[1]')
+        nicknames_form.submit()
+        driver.save_screenshot("kidsnote_imgs/5_kids_note_click_nick_name_selector.png")
 
-    print('5. 호칭 선택')
-    nicknames = driver.find_elements_by_class_name("form-select-nickname")
-    nicknames_form = nicknames[10].find_element_by_tag_name("form")
-    nicknames_form.submit()
-    driver.save_screenshot("kidsnote_imgs/5_kids_note_click_nick_name_selector.png")
-
-    # 비디오 다운로드
-    video_div = driver.find_element_by_class_name('download-button-wrapper')
-    if video_div:    
+    # 비디오 다운로드    
+    if check_exists_by_xpath("//div[@class='download-button-wrapper']"):    
         print('6. 비디오 다운로드')
-        video_a = video_div.find_element_by_tag_name("a")
+        video_a = driver.find_element_by_xpath("//div[@class='download-button-wrapper']//a")
         print(video_a.get_attribute('innerHTML'))
         video_a.click()
         paths = WebDriverWait(driver, 120, 1).until(every_downloads_chrome)
@@ -100,17 +112,21 @@ for item in items:
     img_grid_container = driver.find_element_by_id("img-grid-container")
     if img_grid_container:
         print('7. 모든 이미지 다운로드')
-        img_a_tags = img_grid_container.find_elements_by_tag_name("a")
-        idx = 0
+        img_a_tags = driver.find_elements_by_xpath('//div[@id="img-grid-container"]//div[@class="grid"]')
+        idx = 1
         for img_a_tag in img_a_tags:
-            img_grid_container.find_elements_by_tag_name("a")[idx].click()
+            driver.find_element_by_xpath('//div[@id="img-grid-container"]//div['+str(idx)+']//a').click()
             org_img_tag=driver.find_element_by_xpath("//a[contains(text(),'원본')]")
             org_img_tag.click()
             WebDriverWait(driver, 120, 1).until(every_downloads_chrome)
-            idx = idx+1
-            driver.back()            
-
-    driver.implicitly_wait(10)        
+            idx += 1
+            driver.back()      
+                  
+    driver.implicitly_wait(10)            
+    driver.back()
+    if album_idx <= 1:
+        driver.back()
+    album_idx += 1
 
 
 
