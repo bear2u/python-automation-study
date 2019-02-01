d75e9bc160d29695b3d86da6d32f118840f44954
김태현 <bear2u@naver.com>
2019-02-01 22:06:00 +0900

    add cafelist.py

---
+++ games/main.py
@@ -0,0 +1,15 @@
+from PIL import ImageGrab
+import os
+import time
+ 
+def screenGrab():
+    box = (157,346,796,825)
+    im = ImageGrab.grab(box)
+    im.save(os.getcwd() + '\\full_snap__' + str(int(time.time())) +
+'.png', 'PNG')
+ 
+def main():
+    screenGrab()
+ 
+if __name__ == '__main__':
+    main()
